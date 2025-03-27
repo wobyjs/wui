e497b69a28de6fb56da85aab8c860e0d4241bb93
jeehan <tjh@test.com>
2025-03-27 11:40:42 +0800

    Restored changes to numberField

--- src/NumberField.tsx
+++ src/NumberField.tsx
@@ -1,4 +1,4 @@
-import { $, $$, useEffect, isObservable, useTimeout, useInterval, Observable, ObservableMaybe, type JSX } from 'woby'
+import { $, $$, useEffect, isObservable, useTimeout, useInterval, ObservableMaybe, type JSX } from 'woby'
 
 
 const btn = `
@@ -13,87 +13,83 @@ type NumberFieldProps = JSX.InputHTMLAttributes<HTMLInputElement> & {
 	children?: JSX.Child
 	reactive?: ObservableMaybe<boolean>
 	noMinMax?: ObservableMaybe<boolean>
+	noFix?: ObservableMaybe<boolean>
+	noRotate?: ObservableMaybe<boolean>
 }
 
 export const NumberField = (props: NumberFieldProps): JSX.Element => {
-	const { className, class: cls, children, onChange, noMinMax, onKeyUp, reactive, ...otherProps } = props
-	const { min, max, value, disabled } = otherProps
+	const { className, class: cls, children, onChange, noMinMax, onKeyUp, reactive, noRotate = false, noFix, ...otherProps } = props
+	const { min, max, value, step, disabled } = otherProps
 	const inputRef = $<HTMLInputElement>()
 	const error = () => $$(value) < $$(min) || $$(value) > $$(max)
-	const checkMinValue = () => $$(value) <= $$(min)
-	const checkMaxValue = () => $$(value) >= $$(max)
+	const cantMin = () => $$(value) <= $$(min) && $$(noRotate)
+	const cantMax = () => $$(value) >= $$(max) && $$(noRotate)
 
-    let pvalue: number
-    const updated = () => {
-        if (pvalue === +$$(value)) return
-        if ($$(noFix) && $$(noRotate)) return
+	let pvalue: number
+	const updated = () => {
+		if (pvalue === +$$(value)) return
+		if ($$(noFix) && $$(noRotate)) return
 
-        if (+$$(value) < +$$(min))
-            isObservable(value) && value($$(noRotate) ? +$$(min) : +$$(max))
+		if (+$$(value) < +$$(min))
+			isObservable(value) && value($$(noRotate) ? +$$(min) : +$$(max))
 
-        if (+$$(value) > +$$(max))
-            isObservable(value) && value($$(noRotate) ? +$$(max) : +$$(min))
+		if (+$$(value) > +$$(max))
+			isObservable(value) && value($$(noRotate) ? +$$(max) : +$$(min))
 
-        pvalue = +$$(value)
-    }
+		pvalue = +$$(value)
+	}
 
-    useEffect(updated)
+	useEffect(updated)
 
-    const dec = () => { !$$(reactive) && isObservable(value) ? value?.((+$$(inputRef).value as any) - +$$(step)) : undefined; updated() }
-    const inc = () => { !$$(reactive) && isObservable(value) ? value?.((+$$(inputRef).value as any) + +$$(step)) : undefined; updated() }
+	const dec = () => { !$$(reactive) && isObservable(value) ? value?.((+$$(inputRef).value as any) - +$$(step)) : undefined; updated() }
+	const inc = () => { !$$(reactive) && isObservable(value) ? value?.((+$$(inputRef).value as any) + +$$(step)) : undefined; updated() }
 
-    let interval: ReturnType<typeof useInterval>
-    let timeout: ReturnType<typeof useTimeout>
+	let interval: ReturnType<typeof useInterval>
+	let timeout: ReturnType<typeof useTimeout>
 
-    function startContinuousUpdate(isIncrement: boolean) {
-        // Update immediately on press
-        isIncrement ? inc() : dec()
+	function startContinuousUpdate(isIncrement: boolean) {
+		// Update immediately on press
+		isIncrement ? inc() : dec()
 
-        // Start interval to continue updating while pressed
-        timeout = useTimeout(() => interval = useInterval(() => isIncrement ? inc() : dec(), 100), 200)
-    }
+		// Start interval to continue updating while pressed
+		timeout = useTimeout(() => interval = useInterval(() => isIncrement ? inc() : dec(), 100), 200)
+	}
 
-    function stopUpdate() {
-        timeout?.()
-        interval?.()
-    }
+	function stopUpdate() {
+		timeout?.()
+		interval?.()
+	}
 
-    return <div class={["number-input inline-flex border-2 border-solid border-[#ddd] box-border [&_*]:box-border", className, cls]}>
-        <button class={btn} onPointerDown={() => startContinuousUpdate(false)} onPointerUp={stopUpdate} onPointerLeave={stopUpdate} disabled={cantMin}>-</button>
-        <input ref={inputRef} class={[`quantity  [-webkit-appearance:textfield] [-moz-appearance:textfield] [appearance:textfield]
+	return <div class={["number-input inline-flex border-2 border-solid border-[#ddd] box-border [&_*]:box-border", className, cls]}>
+		<button
+			class={btn}
+			onPointerDown={() => startContinuousUpdate(false)}
+			onPointerUp={stopUpdate}
+			onPointerLeave={stopUpdate}
+			disabled={cantMin}>
+			-
+		</button>
+		<input
+			ref={inputRef}
+			class={[`quantity  [-webkit-appearance:textfield] [-moz-appearance:textfield] [appearance:textfield]
         [&::-webkit-inner-spin-button]:[-webkit-appearance:none] [&::-webkit-outer-spin-button]:[-webkit-appearance:none]
         text-center p-2 border-solid border-[0_2px]
-        `,
-					() => ($$(error) ? "text-[red]" : ""),
-				]}
-				type="number"
-				value={value}
-				{...otherProps}
-				onChange={(e) => {
-					!$$(reactive) && isObservable(value) ? (value?.(e.target.valueAsNumber), onChange?.(e)) : onChange?.(e)
-				}}
-				onKeyUp={(e) => {
-					!$$(reactive) && isObservable(value) ? (value?.(e.target.valueAsNumber), onKeyUp?.(e)) : onKeyUp?.(e)
-				}}
-				onWheel={(e) => {
-					e.preventDefault()
-					!$$(reactive) && isObservable(value) ? value?.(Math.sign(e.deltaY) > 0 ? ++e.target.value : --e.target.value) : undefined
-				}}
-				disabled={disabled}
-			/>
-			<button
-				class={[btn, "plus"]}
-				onClick={(e) => {
-					e.stopImmediatePropagation()
-					$$(inputRef).stepUp()
-					isObservable(value) && value?.($$(inputRef).valueAsNumber)
-					//@ts-ignore TODO
-					// onChange?.($$(inputRef))
-				}}
-				disabled={() => checkMaxValue() || disabled}
-			>
-				+
-			</button>
-		</div>
-	)
+        `, () => ($$(error) ? "text-[red]" : "")]}
+			type="number"
+			value={value}
+			onChange={e => { !$$(reactive) && isObservable(value) ? (value?.(e.target.value), onChange?.(e)) : undefined; updated() }}
+			onKeyUp={e => { !$$(reactive) && isObservable(value) ? (value?.(e.target.value), onKeyUp?.(e)) : undefined; updated() }}
+			onWheel={e => { e.preventDefault(); Math.sign(e.deltaY) > 0 ? dec() : inc() }}
+			{...otherProps}
+			disabled={disabled}
+		/>
+		<button
+			class={[btn, "plus"]}
+			onPointerDown={() => startContinuousUpdate(true)}
+			onPointerUp={stopUpdate}
+			onPointerLeave={stopUpdate}
+			disabled={cantMax}>
+			+
+		</button>
+	</div>
 }
