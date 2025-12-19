d99312d4525bf1ae96cad937ec08b356a88eae6f
JenSien <jensien@mapinfo.com.my>
2025-12-19 15:45:26 +0800

    style(icons): update SVG components to use scalable and consistent styles
    - Change fixed 24px dimensions to scalable 1em for width and height
    - Update fill color to use currentColor for better CSS control
    - Refactor JSX formatting for better readability and consistency
    - Add new plus icon component with stroke attributes and scalable size
    - Keep existing styling and classes unchanged except formatting adjustments

--- src/icons/align_center.tsx
+++ src/icons/align_center.tsx
@@ -1,5 +1,11 @@
 
 import { type JSX } from 'woby'
-export default (props: JSX.SVGAttributes<SVGElement>) => <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"  {...props}>
-    <path d="M120-120v-80h720v80H120Zm160-160v-80h400v80H280ZM120-440v-80h720v80H120Zm160-160v-80h400v80H280ZM120-760v-80h720v80H120Z" />
-</svg>
+export default (props: JSX.SVGAttributes<SVGElement>) =>
+    <svg xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 -960 960 960"
+        height="1em"
+        width="1em"
+        fill="currentColor" //"#5f6368"
+        {...props}>
+        <path d="M120-120v-80h720v80H120Zm160-160v-80h400v80H280ZM120-440v-80h720v80H120Zm160-160v-80h400v80H280ZM120-760v-80h720v80H120Z" />
+    </svg>
